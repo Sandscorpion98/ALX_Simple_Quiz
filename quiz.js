507aec56d95a753da07dab2fd526edc5ff9e5c28
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-07-13 15:48:36 +0300

    Added The Answer For The Quiz.js File

---
+++ quiz.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+function checkAnswer() {
+    const correctAnswer = "4";
+    document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+     
+    if (correctAnswer == userAnswer) {
+    
+        document.getElementById("feedback").textContent = "Correct! Well done.";
+    }
+    else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+    }
+}
+
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer)
+
